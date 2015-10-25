519e163010666a3586f2393854c56953bb7f7fb7
babachir <user.name>
2015-10-25 14:23:56 +0100

    carte

---
+++ exogeo.js
@@ -0,0 +1,8 @@
+	var region = ['Nord-Pas-de-Calais','Picardie','Haute-Normandie','Basse-Normandie','Île-de-France','Bretagne','Champagne-Ardenne','Alsace','Centre','Bourgogne','Rhône-Alpes','Midi-Pyrénées','Languedoc-Roussillon','Poitou-Charentes','Limousin','Franche-Comté' ];
+
+	var regionChoisi;
+
+	function initExo()
+	{
+
+	}
